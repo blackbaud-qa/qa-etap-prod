8e45744bfd5529894c697d6adc8c26579fa258d0
Blackbaud-KristinLawrence <kristin.lawrence@blackbaud.com>
2014-11-11 10:43:25 -0500

    various testing items

---
+++ features/step_definitions/step_remove_dashboard_tile.rb
@@ -0,0 +1,15 @@
+Given(/^the Campaign Performance tile exists on my Dashboard$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click the x in the tile$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+And(/^click Ok$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the tile is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
