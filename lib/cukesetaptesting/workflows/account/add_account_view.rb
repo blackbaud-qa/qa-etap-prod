19f962886e592983adc33e768a415b9362bc0c6e
Blackbaud-KimberlyHammer <kimberly.hammer@blackbaud.com>
2015-01-28 17:31:45 -0500

    add account work

--- lib/cukesetaptesting/workflows/account/add_account_view.rb
+++ lib/cukesetaptesting/workflows/account/add_account_view.rb
@@ -2,6 +2,10 @@ module Cukesetaptesting
   module Account
     class AddAccountView < BaseView
       keyword(:add_account_element)  { content.form(:name, 'addAccountWizardForm') }
+      keyword(:new_account_classic_name) {content.text_field(:id,'name_0')}
+      keyword(:find_screen_add_account) {content.button(:value,'Add Account')}
+      keyword(:new_account_classic_sort_name) {content.text_field(id, 'sortKey_0')}
+      keyword(:new_account_classic_address_lines) {content.text_field(:id, 'address_0')}
 
       def home(model)
       end
