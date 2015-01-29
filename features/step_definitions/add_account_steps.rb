9920d2175ff159f025fa6e64161d0e146b92e480
Blackbaud-KimberlyHammer <kimberly.hammer@blackbaud.com>
2015-01-29 14:59:26 -0500

    add account work continued

--- features/step_definitions/add_account_steps.rb
+++ features/step_definitions/add_account_steps.rb
@@ -8,13 +8,37 @@ And (/^I set Name to '([^']*)' on the classic add account page/) do |name|
   accounts.create
 end
 
-And (/^I set Sort Name to 'Doe, John' on the classic add account page/) do |name|
+And (/^I set Sort Name to '([^']*)' on the classic add account page/) do |name|
   accounts = Account::AddAccount.new(:new_account_classic_sort_name => name)
   accounts.create
 end
 
-
-And (/^I set Address Lines to '501 N Pennsylvania Parkway' on the classic add account page/) do |name|
+And (/^I set Address Lines to '([^']*)' on the classic add account page/) do |name|
   accounts = Account::AddAccount.new(:new_account_classic_address_lines => name)
+  accounts.create
+end
+
+And (/^I set City to '([^']*)' on the classic add account page/) do |name|
+  accounts = Account::AddAccount.new(:new_account_classic_city => name)
+  accounts.create
+end
+
+And (/^I set State to '([^']*)' on the classic add account page/) do |name|
+  accounts = Account::AddAccount.new(:new_account_classic_state => name)
+  accounts.create
+end
+
+And (/^I set Postal Code to '([^']*)' on the classic add account page/) do |name|
+  accounts = Account::AddAccount.new(:new_account_classic_postal_code => name)
+  accounts.create
+end
+
+And (/^I set County to '([^']*)' on the classic add account page/) do |name|
+  accounts = Account::AddAccount.new(:new_account_classic_postal_county => name)
+  accounts.create
+end
+
+And (/^I set Voice to '([^']*)' on the classic add account page/) do |name|
+  accounts = Account::AddAccount.new(:new_account_classic_voice => name)
   accounts.create
 end
