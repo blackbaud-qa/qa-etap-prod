87254992c570aab545a0529525357ca8ef6acc04
Lance Moore <lance.moore@blackbaud.com>
2015-03-23 12:06:08 -0400

    Adding step defs for new doc guided 1

--- lib/cukesetaptesting/workflows/communications/createtemplate_view.rb
+++ lib/cukesetaptesting/workflows/communications/createtemplate_view.rb
@@ -27,13 +27,15 @@ module Cukesetaptesting
       keyword(:font_family_comic_sans) {content.div(:id,'mceu_110')}
       keyword(:formats) {content.div(:id,'mceu_8')}
       keyword(:formats_12pt) {content.div(:id,'mceu_130')}
-      keyword(:new_template_footer) {comm_template.div(:id,'guideFooter')}
+      # keyword(:new_template_footer) {comm_template.div(:id,'guideFooter')}
+      keyword(:new_template_footer) {comm_template.div(:class,'block_closing')}
       keyword(:new_template_select) {comm_template.select(:id,'nextMapping')}
       keyword(:new_template_save) {comm_template.a(:text,'Save and')}
       keyword(:new_template_settings) {comm_template.a(:text,'Settings')}
       keyword(:new_template_edit_guide) {content.p(:class,'hoverControls')}
       keyword(:new_template_edit_guide_delete) {new_template_edit_guide.a(:class,'delete').img}
-      keyword(:new_template_edit_guide_confirm_yes) {browser.div(:class,'popupWindow').input(:value,'Yes')}
+
+      keyword(:new_template_edit_guide_confirm_yes) {content.button(:value,'Yes')}
       # keyword(:new_template_save) {comm_template.tr(:id,'wizardWorkflowRow_3_taskWorkflow').img}
       keyword(:gen_letters_category) {letter_guide.select(:id,'select.query.category.1')}
       keyword(:gen_letters_query) {letter_guide.select(:id,'select.query.1')}
@@ -46,6 +48,29 @@ module Cukesetaptesting
       keyword(:correspondence_cat_receipts) {comms_categories.a(:text,'Receipts')}
       keyword(:new_template_reciept_stub_bottom) {comm_template.div(:id,'tabContentlayoutsdocuments').label(:text,'Receipt w/ Stub (Bottom)').parent.a}
       keyword(:new_template_delete_section) {content.div(:id,'letterGuideClickToEdit').div.p(:class,'hoverControls').a(:class,'delete')}
+      keyword(:new_template_add_block) {content.select(:id,'addBlockSelector')}
+      keyword(:new_template_body) {content.div(:class,'block_body')}
+      keyword(:new_template_text) {content.iframe(:id,'blockHtmlContents_ifr')}
+      keyword(:new_template_long_salutation) {new_template_text.a(:text,'Long Salutation')}
+      # keyword(:new_template_insert_merge) {content.div(:id,'mceu_26')}
+      keyword(:merge_tags_field) {content.div(:class,'mce-reset').iframe.div(:id,'mergeTagsWrapper').select_list(:name,'field')}
+      keyword(:merge_tags_insert) {content.div(:class,'mce-reset').iframe.div(:id,'mergeTagsWrapper').button(:id,'insert')}
+      keyword(:new_template_font) {content.div(:id,'mceu_7')}
+      keyword(:new_template_font_tnr) {content.div(:id,'mceu_91').span}
+      keyword(:new_template_font_size) {content.div(:id,'mceu_8')}
+      keyword(:new_template_font_12) {content.div(:id,'mceu_102').span}
+      keyword(:template_pop_up_update) {content.div(:id,'blockHtmlEdit').parent.parent.div(:class,'popFooter').input(:value,'Update')}
+      keyword(:template_save_and_create) {content.select(:id,'nextMapping')}
+      keyword(:template_save_and) {content.a(:text,'Save and')}
+      keyword(:template_query_category) {content.select(:name,'sourceCategory')}
+      keyword(:template_list) {content.ul(:id,'list_templates')}
+      keyword(:new_template_query) {content.select(:name,'sourceQuery')}
+      keyword(:new_template_next_content) {content.div(:class,'taskContentRight')}
+      keyword(:new_template_receipt_next) {content.input(:value,'Next')}
+      keyword(:new_template_doc_type_word) {content.radio(:id,'docxExt')}
+      keyword(:new_template_grouping_one_per) {content.radio(:value,'label.none')}
+
+
 
       def home(model)
       end
