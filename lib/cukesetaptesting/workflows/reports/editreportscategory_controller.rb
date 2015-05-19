d30688282bfbd8d9506b5def30a7ae0077e7b8de
Blackbaud-HusnaSiddiqui <husna.siddiqui@blackbaud.com>
2015-05-19 14:14:37 -0400

    export.feature completed

--- lib/cukesetaptesting/workflows/reports/editreportscategory_controller.rb
+++ lib/cukesetaptesting/workflows/reports/editreportscategory_controller.rb
@@ -22,16 +22,44 @@ module Cukesetaptesting
         @view.query_all_constituents.when_present.click
       end
 
+
       #export
+      def select_download_format_options
+        @view.report_format.when_present.click
+      end
 
-
-      def download_as(type)
-        @view.report_format.select(type)
+      def select_download_format format
+        @view.report_format.when_present.select format
       end
 
       def see_test_report?(title)
         @view.page_title.when_present.text.include?(title)
       end
+
+      #verify download methods
+      def downloaded_as_pdf?(format)
+        @view.downloaded_as_pdf.wait_until_present
+        @view.downloaded_as_pdf.when_present.text.include?(format)
+      end
+
+      def in_dropbox_as?(format)
+        @view.dropbox_list.when_present.text.include?(format)
+      end
+
+      def report_generating?
+        @view.generating_report.when_present.text.include? 'Test Report'
+      end
+
+      def report_sent_as_email?
+        @view.report_sent.when_present.text.include? 'inbox'
+      end
+
+      #report_load_copy_move_delete
+
+
+
+
+
 
     end
   end
--- lib/cukesetaptesting/workflows/reports/editreportscategory_view.rb
+++ lib/cukesetaptesting/workflows/reports/editreportscategory_view.rb
@@ -24,11 +24,18 @@ module Cukesetaptesting
       keyword(:category_base) {content.select(:id, 'select.query.category.1').option(:text,'Base')}
       keyword(:query_all_constituents) {content.select(:id, 'select.query.1').option(:text,'All Constituents - A')}
 
-      keyword(:report_format) {content.div(:id, 'deliveryOptions').select(:id, 'modeSelection')}
+      keyword(:report_format)  { content.select_list(:id,'modeSelection') }
       keyword(:page_title) {content.div(:class, 'pageTitle')}
 
       #export
+      keyword(:downloaded_as_pdf) {content.div(:id, 'pdfDownloadMessage').b}
+      keyword(:dropbox_list) {content.ul(:id, 'list_dropBox').li}
+      keyword(:generating_report) {content.div(:id, 'messageArea').div}
+      keyword(:report_sent) {content.div(:class, 'contentBodyDiv').b}
 
+
+      #report_load_copy_move_delete
+      keyword(:new_category)
 
     end
   end
