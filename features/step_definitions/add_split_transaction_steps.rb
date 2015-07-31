765c56555ea16fb41bac08cba15aa25789d7f588
Blackbaud-KimberlyHammer <kimberly.hammer@blackbaud.com>
2015-07-31 15:09:18 -0400

    Finished add_split_transaction feature file

--- features/step_definitions/add_split_transaction_steps.rb
+++ features/step_definitions/add_split_transaction_steps.rb
@@ -152,12 +152,115 @@ And (/the Approach on Segment 1 should be set to '([^']*)'/) do |value|
   expect(gift.segment_one_approach).to eq(value)
 end
 
-And (/the Gift Type on Segment 1 should be set to '([^']*)'/) do |value|
-  gift = Giving::GiftPledge.new
-  expect(gift.segment_one_gift_type).to eq(value)
-end
-
 And (/the Check Date on Segment 1 should be set to '([^']*)'/) do |value|
   gift = Giving::GiftPledge.new
   expect(gift.segment_one_check_date).to eq(value)
+end
+
+And (/the Check Number on Segment 1 should be set to '([^']*)'/) do |value|
+  gift = Giving::GiftPledge.new
+  expect(gift.segment_one_check_number).to eq(value)
+end
+
+And(/^the Tribute Information on Segment 1 should be set to '([^']*)'$/) do |tribute|
+  gift = Giving::GiftPledge.new
+  gift.tribute_bar_click_segment_one
+  expect(gift.segment_one_tribute? tribute).to eq(true)
+end
+
+And(/^the Soft Credit Information on Segment 1 should be set to '([^']*)'$/) do |value|
+  gift = Giving::GiftPledge.new
+  expect(gift.segment_one_soft_credit_name? value).to eq(true)
+end
+
+And(/^the Soft Credit Amount on Segment 1 should be set to '([^']*)'$/) do |value|
+  gift = Giving::GiftPledge.new
+  expect(gift.segment_one_soft_credit_amount).to eq(value)
+end
+
+And (/the Received amount on Segment 2 should be set to '([^']*)'/) do |amount|
+  gift = Giving::GiftPledge.new
+  expect(gift.segment_two_received_amount).to eq(amount)
+end
+
+And (/the Fund on Segment 2 should be set to '([^']*)'/) do |value|
+  gift = Giving::GiftPledge.new
+  expect(gift.segment_two_fund).to eq(value)
+end
+
+And (/the Campaign on Segment 2 should be set to '([^']*)'/) do |value|
+  gift = Giving::GiftPledge.new
+  expect(gift.segment_two_campaign).to eq(value)
+end
+
+And (/the Approach on Segment 2 should be set to '([^']*)'/) do |value|
+  gift = Giving::GiftPledge.new
+  expect(gift.segment_two_approach).to eq(value)
+end
+
+And(/^the Tribute Information on Segment 2 should be set to '([^']*)'$/) do |tribute|
+  gift = Giving::GiftPledge.new
+  gift.tribute_bar_click_segment_two
+  expect(gift.segment_two_tribute? tribute).to eq(true)
+end
+
+And(/^the Soft Credit Information on Segment 2 should be set to '([^']*)'$/) do |value|
+  gift = Giving::GiftPledge.new
+  expect(gift.segment_two_soft_credit_name? value).to eq(true)
+end
+
+And(/^the Soft Credit Amount on Segment 2 should be set to '([^']*)'$/) do |value|
+  gift = Giving::GiftPledge.new
+  expect(gift.segment_two_soft_credit_amount).to eq(value)
+end
+
+And (/^the '([^']*)' UDF on Segment 2 should be set to '([^']*)'$/) do |udf, value|
+  gift = Giving::GiftPledge.new
+  gift.udf_bar_click_segment_two
+  expect(gift.segment_two_udf(udf)).to eq(value)
+end
+
+And(/^I click on the split transaction in the journal/) do
+  gift = Giving::GiftPledge.new
+  gift.journal_split_transaction
+end
+
+And(/^I set the Received amount on Segment 3 to '([^']*)'/) do |amount|
+  gift = Giving::GiftPledge.new(:segment_three_received_amount=>amount)
+  gift.create
+end
+
+And(/^I set the Fund on Segment 3 to '([^']*)'/) do |name|
+  gift = Giving::GiftPledge.new
+  gift.set_fund_segment_three name
+end
+
+And(/^I set the Gift Type on Segment 3 to '([^']*)'/) do |value|
+  gift = Giving::GiftPledge.new
+  gift.set_gift_type_segment_three value
+end
+
+And(/^I set the Check Date on Segment 3 to '([^']*)'/) do |date|
+  gift = Giving::GiftPledge.new(:segment_three_check_date=>date)
+  gift.create
+end
+
+And(/^I set the Check Number on Segment 3 to '([^']*)'/) do |value|
+  gift = Giving::GiftPledge.new(:segment_three_check_number=>value)
+  gift.create
+end
+
+And(/^I click Delete Segment 3/) do
+  gift = Giving::GiftPledge.new
+  gift.delete_segment_three
+end
+
+And(/^I change the Received Amount Field to '([^']*)'/) do |amount|
+  gift = Giving::GiftPledge.new(:edit_split_received_field=>amount)
+  gift.create
+end
+
+And(/^I change the Number Of Segments to '([^']*)'/) do |segments|
+  gift = Giving::GiftPledge.new()
+  gift.edit_segment(segments)
 end
