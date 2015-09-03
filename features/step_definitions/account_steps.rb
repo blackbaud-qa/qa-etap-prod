bed46e4af97a0b60315a41eaf17acf893912c5a6
Blackbaud-MattDilts <matt.dilts@blackbaud.com>
2015-09-03 15:04:16 -0400

    Clean up superfluous comments
    Clean up superfluous comments

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -95,15 +95,10 @@ When (/^delete the accounts$/) do
     ]
 
     if search.account_name_exists? row[1]+' '+row[2]+' '+row[3]
-      pp "000"
       steps %Q[And I click on '#{row[1]} #{row[2]} #{row[3]}' on the accounts page]
-      pp "1111"
       steps %Q[  And I click on the account settings page on the accounts page]
-      pp "22222"
       steps %Q[And I click Delete Role on the Account Settings page]
-      pp "333333"
       steps %Q[And I click Yes on the Account Settings page]
-      pp "4444444"
     end
 
     sleep 0.3
@@ -117,39 +112,22 @@ When (/^add the accounts$/) do
   ]
 
   CSV.foreach("C:\\users\\matt.dilts\\Desktop\\test4kc.csv") do |row|
-    pp "0"
     steps %Q[When I set Title to '#{row[0]}' on the classic add account page]
-    pp "11"
     steps %Q[And I set First Name to '#{row[1]}' on the classic add account page]
-    pp "222"
     steps %Q[And I set Middle Name to '#{row[2]}' on the classic add account page]
-    pp "3333"
     steps %Q[And I set Last Name to '#{row[3]}' on the classic add account page]
-    pp "44444"
     steps %Q[And I set Address Lines to '#{row[4]}' on the classic add account page]
-    pp "555555"
     steps %Q[And I set City to '#{row[5]}' on the classic add account page]
-    pp "6666666"
     steps %Q[And I set State to '#{row[6]}' on the classic add account page]
-    pp "77777777"
     steps %Q[And I set Postal Code to '#{row[8]}' on the classic add account page]
-    pp "888888888"
     steps %Q[And I set Voice to '#{row[10]}' on the classic add account page]
-    pp "9999999999"
     steps %Q[And I set Email to '#{row[9]}' on the classic add account page]
-    pp "aaa"
     steps %Q[And I set Note to '#{row[1]} #{row[3]}' on the classic add account page]
-    pp "bbb"
     steps %Q[And I set the UDF 'Account Type' dropdown to 'Individual' on the classic add account page]
-    pp "ccc"
     steps %Q[And I set the UDF 'Gender' dropdown to '#{row[12]}' on the classic add account page]
-    pp "ddd"
     steps %Q[And I set the UDF 'Date of Birth' to '#{row[11]}' on the classic add account page]
-    pp "eee"
     steps %Q[And I click Save And 'New']
-    pp "fff"
     steps %Q[And I handle the duplicate report if necessary]
-    pp "ggg"
 
     sleep 0.3
   end
@@ -246,23 +224,4 @@ When (/^I create user '([^']*)' with password '([^']*)'$/) do |user_name, passwo
   account.set_user_password password #user_name
   account.set_user_password_confirm password #user_name
   step %Q[I click Save And '#{desired_next_page}'] # eg: 'Go to Personas'
-
-  # step %Q[I am logged into eTap]
-  # step %Q[I click Accounts]
-  # step %Q[I click on Add Account on the find account screen]
-  # step %Q[I set Name to '#{account_name}' on the classic add account page]
-  # step %Q[I set Sort Name to '#{sort_name}' on the classic add account page]
-  # step %Q[I set Address Lines to '#{address}' on the classic add account page]
-  # step %Q[I set City to '#{city}' on the classic add account page]
-  # step %Q[I set State to '#{state}' on the classic add account page]
-  # step %Q[I set Postal Code to '#{postal_code}' on the classic add account page]
-  # step %Q[I set County to '#{county}' on the classic add account page]
-  # step %Q[I set Voice to '#{voice}' on the classic add account page]
-  # step %Q[I set Email to '#{email}' on the classic add account page]
-  # step %Q[I set Web Page to '#{web_page}' on the classic add account page]
-  # step %Q[I set Note to '#{note}' on the classic add account page]
-  # step %Q[I set Short Salutation to '#{short_sal}' on the classic add account page]
-  # step %Q[I set Long Salutation to '#{long_sal}' on the classic add account page]
-  # step %Q[I set the UDF '#{udf_name}' to '#{udf_value}' on the classic add account page]
-  # step %Q[I click Save And '#{desired_next_page}'] # eg: 'Go to Personas'
 end
