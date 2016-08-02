1e99276696db08a0340815d9effd704a3644702b
Blackbaud-LanceMoore <lance.moore@blackbaud.com>
2016-08-02 06:55:47 -0400

    Updating DIY timeout

--- features/step_definitions/diy_steps.rb
+++ features/step_definitions/diy_steps.rb
@@ -327,7 +327,7 @@ end
 And(/^I click Replace on the DIY editor page$/) do
   diy = DIY::Onlineforms.new()
   diy.donation_page_replace_click
-  sleep 10
+  sleep 30
 end
 
 Then(/the Donation Page should no longer show$/) do
